abac2f4e37a81962d0351cd44fcfc4ba0781c58b
Jaeyun Shim <tlawodbs1170@unist.ac.kr>
2023-07-11 06:13:38 +0900

    Make iMB-basic except apply pruning strategies (Lemma 3 and Lemma 6)

--- algorithm/biplex.py
+++ algorithm/biplex.py
@@ -5,7 +5,6 @@ def iMB_Basic(G, k, t, X, N_X, cand_p, cand_q):
 
     while(len(cand_p) != 0):
         u = cand_p[0]
-        expand = True
 
         new_X = X.union({u})
         cand_p.remove(u)
@@ -13,10 +12,15 @@ def iMB_Basic(G, k, t, X, N_X, cand_p, cand_q):
         new_cand_p = cand_p
         # Apply the early stop strategy (Lemma 2) 만약 cand_q 에 있는 u에 대해서 N_X가 u의 이웃들을 담은 set에 포함될 떄, expand stop
         for vertex in cand_q:
-            if N_X.issubset(set(G.neighbors(vertex))):
-                expand = False
+            if set(N_X).issubset(set(G.neighbors(vertex))):
+                continue
 
         # Apply the advanced node expansion (Lemma 4)
+        for vertex in cand_p:
+            if set(N_X).issubset(set(G.neighbors(vertex))):
+                new_X = new_X.union(cand_p)
+                cand_q.update(cand_p)
+                cand_p.clear()
 
         if len(new_X) >= t:
             # List all MBs
@@ -25,19 +29,17 @@ def iMB_Basic(G, k, t, X, N_X, cand_p, cand_q):
                 print(new_N_X)
             # Apply pruning strategies (Lemma 3 and Lemma 6)
 
-        if len(new_X) + len(cand_p) >= t and len(new_N_X) >= t and expand:
+        if len(new_X) + len(cand_p) >= t and len(new_N_X) >= t:
             # print("test")
             iMB_Basic(G, k, t, new_X, new_N_X, new_cand_p, cand_q)
 
-        cand_q.union({u})
+        cand_q.add(u)
         # print(new_X)
         # print(cand_p)
         # print(new_N_X)
         # print(new_cand_p)
+        # print(cand_q)
         # iMB_Basic()
-
-
-
     return nx.connected_components(G)
 
 def extract_number(s):
@@ -47,8 +49,8 @@ def run(G_, k, t): #t is threshold
     G = G_.copy()
 
     X = set()  # 이게 X에 해당하는 vertices, 처음에는 비어있음
-    N_X = set(v for v, d in G.nodes(data = True) if d['bipartite'] == 1)  # 이게 Y에 해당하는 vertices, 처음에 모든 v로 채워둬야 됨
-    cand_p = set(u for u, d in G.nodes(data = True) if d['bipartite'] == 0)  # X에 들어갈 수 있는 vertices, 처음에 모든 u로 채워둬야 됨
+    N_X = set(v for v, d in G.nodes(data=True) if d['bipartite'] == 1)  # 이게 Y에 해당하는 vertices, 처음에 모든 v로 채워둬야 됨
+    cand_p = set(u for u, d in G.nodes(data=True) if d['bipartite'] == 0)  # X에 들어갈 수 있는 vertices, 처음에 모든 u로 채워둬야 됨
     cand_q = set()  # 이미 방문한 vertices
 
     N_X = sorted(N_X, key=extract_number)  #sorting vertices
