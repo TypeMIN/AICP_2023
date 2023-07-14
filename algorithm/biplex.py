c88634e86c526e1a234d91fcfff04190c24cc583
tlawodbs <tlawodbs1170@unist.ac.kr>
2023-07-14 23:28:45 +0900

    Fix biplex.py

--- algorithm/biplex.py
+++ algorithm/biplex.py
@@ -1,74 +1,54 @@
 import networkx as nx
-import itertools
 
 def iMB_Basic(G, k, t, X, N_X, cand_p, cand_q):
 
     while(len(cand_p) != 0):
         u = cand_p[0]
-
-        # print("u", u)
-        # print("X", X)
         new_X = X.union({u})
-        # print("new_X", new_X)
         cand_p.remove(u)
-
         new_cand_p = cand_p.copy()
-
         new_N_X = set()
+
         for v in N_X:
-            neighbors_in_new_X = set(G.neighbors(v)).intersection(new_X)
-            # print(neighbors_in_new_X)
-            if len(neighbors_in_new_X) >= (len(new_X) - k):
+            count = 0
+            for neighbor in G.neighbors(v):
+                if neighbor in new_X:
+                    count = count + 1
+            if count >= (len(new_X) - k) and count > 0: #  count > 0 인 조건을 넣어야 하나? len(new_X) - k 가 음수로 가면 new_N_X가 이상해진다.
                 new_N_X.add(v)
 
-        # print("new_N_X", new_N_X)
-        # print("new_cand_p", new_cand_p)
-        # Apply the early stop strategy (Lemma 2) 만약 cand_q 에 있는 u에 대해서 N_X가 u의 이웃들을 담은 set에 포함될 떄, expand stop
+        # Apply the early stop strategy (Lemma 2)
         for vertex in cand_q:
             if set(N_X).issubset(set(G.neighbors(vertex))):
-                continue
+                print("early stop")
+                cand_q = cand_q.union(new_cand_p)
+                new_cand_p.clear()
+                break
 
         # Apply the advanced node expansion (Lemma 4)
-        # for vertex in cand_p:
-        #     if set(N_X).issubset(set(G.neighbors(vertex))):
-        #         new_X = new_X.union(cand_p)
-        #         cand_q.update(cand_p)
-        #         cand_p.clear()
-
+        for vertex in new_cand_p:
+            if set(new_N_X).issubset(set(G.neighbors(vertex))):
+                print("advanced node expansion")
+                new_X.add(vertex)
+                # cand_q.add(vertex) # 이거 넣어야 하나?
+                new_cand_p.remove(vertex)
+                # break
 
         if len(new_X) >= t:
             # List all bipartite subgraph make by new_X and y, y is subgraph of new_N_X and len(y) >= t
-
             if len(new_N_X) >= t:
-                print("graph")
+                # print("graph")
                 print(new_X)
                 print(new_N_X)
-
-                # n = len(new_N_X)
-                # subsets = []
-                # for r in range(t, n + 1):
-                #     subsets.extend(list(itertools.combinations(new_N_X, r)))
-                # print(subsets)
             # Apply pruning strategies (Lemma 3 and Lemma 6)
 
-
-
-        if len(new_X) + len(cand_p) >= t and len(new_N_X) >= t:
-            # print("test")
+        if len(new_X) + len(new_cand_p) >= t and len(new_N_X) >= t:
             iMB_Basic(G, k, t, new_X, new_N_X, new_cand_p, cand_q)
 
         cand_q.add(u)
-        # print(new_X)
-        # print(cand_p)
-        # print(new_N_X)
-        # print(new_cand_p)
-        # print(cand_q)
-        # iMB_Basic()
-    return nx.connected_components(G)
 
 def coreprune(G_, k, t):
     G = G_.copy()
-    #for each vertex in G, if degree of vertex is less than t - k, remove vertex
     for v in list(G_.nodes()):
         if G_.degree(v) < t - k:
             G.remove_node(v)
@@ -77,9 +57,9 @@ def coreprune(G_, k, t):
 def extract_number(s):
     return int(s[1:])
 
-def run(G_, k, t): #t is threshold
+def run(G_, k, t): #  t is threshold
     G = coreprune(G_.copy(), k, t)
-    #print a bipartite set of G
+
     print(nx.bipartite.sets(G))
 
     X = set()  # 이게 X에 해당하는 vertices, 처음에는 비어있음
@@ -90,6 +70,8 @@ def run(G_, k, t): #t is threshold
     N_X = sorted(N_X, key=extract_number)  #sorting vertices
     cand_p = sorted(cand_p, key=extract_number)
 
-    return iMB_Basic(G, k, t, X, N_X, cand_p, cand_q)
+    iMB_Basic(G, k, t, X, N_X, cand_p, cand_q)
+
+    return nx.connected_components(G)
 
 
