8131c9a5f1082b7e25ef366b71285453d724a832
Jaeyun Shim <tlawodbs1170@unist.ac.kr>
2023-07-20 03:59:54 +0900

    Fix bitruss.py

--- algorithm/bitruss.py
+++ algorithm/bitruss.py
@@ -6,8 +6,12 @@ def count_rectangles(G_):
     # c[e] keeps the number of rectangles in G counting e
     G = G_.copy()
     c = defaultdict(int)
-    for edge in G.edges():
+
+    for edge in list(G.edges()):
         u, v = edge
+        if G.nodes[u]['bipartite'] == 1:
+            u, v = v, u
+
         v_prime = set(G.neighbors(u))
         u_prime = set(G.neighbors(v))
         v_prime.remove(v)
@@ -26,29 +30,38 @@ def run(G_, k):
     G = G_.copy()
     G_r = G_.copy()
     c = count_rectangles(G)
+    # print(c)
     t = {}
     k_ = 0
     # bitruss number 계산
     while len(G.edges()) > 0:
+
         for edge in G.edges():
             u, v = edge
-            if c[edge] <= k_:
+            if G.nodes[u]['bipartite'] == 1:
+                u, v = v, u
+
+            if c[(u, v)] <= k_:
                 for v_prime in G.neighbors(u):
                     for u_prime in G.neighbors(v):
-                        if G.has_edge(u_prime, v_prime):
+                        if G.has_edge(u_prime, v_prime) and u_prime != u and v_prime != v:
                             c[(u, v_prime)] -= 1
                             c[(u_prime, v)] -= 1
                             c[(u_prime, v_prime)] -= 1
-                t[edge] = k_
+                t[(u, v)] = k_
                 G.remove_edge(u, v)
                 break
+
         else:
             k_ += 1
 
-    # k-bitruss 구성
+    # k-bitruss subgraph 구성
     for edge in G_r.edges():
         u, v = edge
-        if t[edge] < k:
+        if G_r.nodes[u]['bipartite'] == 1:
+            u, v = v, u
+
+        if t[(u, v)] < k:
             G_r.remove_edge(u, v)
 
     # edge 없는 node 삭제
